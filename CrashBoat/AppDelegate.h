ffe148bd834d84aba2e8e9f1aff88962c272d944
MR.Lee <759943432@qq.com>
2018-03-06 13:33:35 +0800

    Initial Commit

---
+++ CrashBoat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrashBoat
+//
+//  Created by Mrlee on 2018/3/6.
+//  Copyright © 2018年 Mrlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrashBoat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CrashBoat
+//
+//  Created by Mrlee on 2018/3/6.
+//  Copyright © 2018年 Mrlee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

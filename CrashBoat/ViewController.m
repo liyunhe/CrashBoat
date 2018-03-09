f3a076a0492581dac7a48f7d8f4661676b8ec9be
Anothonny <www.18754186100@163.com>
2018-03-09 15:44:06 +0800

    添加时间判断

--- CrashBoat/ViewController.m
+++ CrashBoat/ViewController.m
@@ -8,9 +8,7 @@
 
 #import "ViewController.h"
 @interface ViewController ()
-{
-     dispatch_source_t _timer;
-}
+
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
@@ -34,54 +32,42 @@
 }
 -(void)startTime
 {
+    //获取当前时间
     NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH-mm-sss"];
+    //获取设备时间
+     NSDate *startDate = [NSDate date];
     
     NSDate *endDate = [dateFormatter dateFromString:[self getyyyymmdd]];
-    NSDate *endDate_tomorrow = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate:([endDate timeIntervalSinceReferenceDate] + 24*3600*11)];
-    NSDate *startDate = [NSDate date];
-    NSTimeInterval timeInterval =[endDate_tomorrow timeIntervalSinceDate:startDate];
+    NSDate *endDate_tomorrow = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate:([endDate timeIntervalSinceReferenceDate])];
+
+    NSTimeInterval timeInterval =[startDate timeIntervalSinceDate:endDate_tomorrow];
     
-    if (_timer==nil) {
-        __block int timeout = timeInterval; //倒计时时间
-        
-        if (timeout!=0) {
-            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-            _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
-            dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
-            dispatch_source_set_event_handler(_timer, ^{
-                if(timeout<=0){ //倒计时结束，关闭
-                    dispatch_source_cancel(_timer);
-                    _timer = nil;
-                    dispatch_async(dispatch_get_main_queue(), ^{
-            
-                    });
-                }else{
-                    int days = (int)(timeout/(3600*24));
-                 
-                    int hours = (int)((timeout-days*24*3600)/3600);
-                    int minute = (int)(timeout-days*24*3600-hours*3600)/60;
-                    int second = timeout-days*24*3600-hours*3600-minute*60;
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        NSLog(@"%d天：%d时：%d分：%d秒",days,hours,minute,second);
-                    });
-                    timeout--;
-                }
-            });
-            dispatch_resume(_timer);
-        }
+    //秒数
+    NSInteger second =(NSInteger)timeInterval;
+    //此处判断即可如果second大于10*24*3600就去执行方法
+    if (second>=10*24*3600) {
+        //可以推送！可以执行方法
+        NSLog(@"推送吧大兄弟");
     }
+    else
+    {
+      //10天倒计时还没到呢！！！
+        NSLog(@"还没到火候！不要急!");
+    }
+  
 }
 
 -(NSString *)getyyyymmdd{
-    NSDate *now = [NSDate date];
     NSDateFormatter *formatDay = [[NSDateFormatter alloc] init];
-    formatDay.dateFormat = @"yyyy-MM-dd";
-    NSString *dayStr = [formatDay stringFromDate:now];
+    formatDay.dateFormat = @"yyyy-MM-dd HH-mm-sss";
+    NSDate *resDate = [formatDay dateFromString:@"2018-3-9 00-00-00"];
+    NSString *dayStr = [formatDay stringFromDate:resDate];
     
     return dayStr;
     
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
